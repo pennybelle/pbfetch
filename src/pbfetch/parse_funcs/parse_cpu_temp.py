ef13edd6a8e01120cc86f3dbff1efc7abec9a420
pennybelle <kylanfreeman98@gmail.com>
2024-08-12 21:38:38 -0400

    specify cpu types, add fallback

--- src/pbfetch/parse_funcs/parse_cpu_temp.py
+++ src/pbfetch/parse_funcs/parse_cpu_temp.py
@@ -2,19 +2,29 @@ from subprocess import run
 
 
 def parse_cpu_temp():
-    for i in range(9):
-        cpu_temp = run(
-            [
-                rf"paste <(cat /sys/class/thermal/thermal_zone{i}/type) <(cat /sys/class/thermal/thermal_zone{i}/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'"
-            ],  #  | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
-            shell=True,
-            capture_output=True,
-        ).stdout
-        if "BAT" not in str(cpu_temp):
-            break
+    try:
+        # check at most 10 times for each
+        #   thermal_zone dir till cpu is found
+        for i in range(9):
+            data = run(
+                [
+                    rf"paste <(cat /sys/class/thermal/thermal_zone{i}/type) <(cat /sys/class/thermal/thermal_zone{i}/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'"
+                ],  #  | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
+                shell=True,
+                capture_output=True,
+            ).stdout
 
-    cpu_temp = str(cpu_temp)
-    cpu_temp = " ".join(cpu_temp.split("\\"))
-    cpu_temp = cpu_temp.split()[1]
+            # omit battery temp from scan
+            data = str(data)
+            if "acpitz" in data or "x86_pkg_temp" in data:
+                break
 
-    return round(float(cpu_temp))
+        cpu_temp = str(data)
+        cpu_temp = " ".join(cpu_temp.split("\\"))
+        cpu_temp = cpu_temp.split()[1]
+
+        return round(float(cpu_temp))
+
+    except Exception as e:
+        print(e)
+        return None
