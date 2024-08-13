79a0de0bc9b59de22f47f4568169f207b4f27ebe
pennybelle <kylanfreeman98@gmail.com>
2024-08-12 21:49:14 -0400

    better logic for cpu temp parsing

--- src/pbfetch/parse_funcs/parse_cpu_temp.py
+++ src/pbfetch/parse_funcs/parse_cpu_temp.py
@@ -3,9 +3,9 @@ from subprocess import run
 
 def parse_cpu_temp():
     try:
-        # check at most 10 times for each
+        # check at most 30 times for each
         #   thermal_zone dir till cpu is found
-        for i in range(9):
+        for i in range(30):
             data = run(
                 [
                     rf"paste <(cat /sys/class/thermal/thermal_zone{i}/type) <(cat /sys/class/thermal/thermal_zone{i}/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'"
@@ -14,11 +14,18 @@ def parse_cpu_temp():
                 capture_output=True,
             ).stdout
 
-            # omit battery temp from scan
+            # return none if cpu isnt found
+            if i == 29:
+                return None
+
+            # only return cpu temp
             data = str(data)
-            if "acpitz" in data or "x86_pkg_temp" in data:
+            if "x86_pkg_temp" in data:
+                break
+            elif "acpitz" in data:
                 break
 
+        # parse string for temp number
         cpu_temp = str(data)
         cpu_temp = " ".join(cpu_temp.split("\\"))
         cpu_temp = cpu_temp.split()[1]
