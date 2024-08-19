eb6cf3d342beb27d2926e0fb68490f9f7533a57e
pennybelle <kylanfreeman98@gmail.com>
2024-08-19 09:21:37 -0400

    parse res from file using LVDS

--- src/pbfetch/parse_funcs/parse_res.py
+++ src/pbfetch/parse_funcs/parse_res.py
@@ -3,13 +3,13 @@ import os
 
 def parse_res():
     try:
-        path = "/sys/class/drm/card"
+        path = "/sys/class/drm"
 
         for i in range(10):
-            if os.path.isdir(f"{path}{i}") is not True:
+            if os.path.isdir(f"{path}/card{i}-LVDS-1") is not True:
                 continue
 
-            with open(f"{path}{i}/card{i}-LVDS-1/modes") as file:
+            with open(f"{path}/card{i}-LVDS-1/modes") as file:
                 data = file.read()
                 return str(data).strip()
 
