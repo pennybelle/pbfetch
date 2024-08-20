d9e923f24f12b36345837f33ccef78befaafe61c
kurealnum <oscar.gaske.cs@gmail.com>
2024-08-20 18:02:17 -0400

    Fixed a hardcoded variable

--- src/pbfetch/main_funcs/stats.py
+++ src/pbfetch/main_funcs/stats.py
@@ -101,7 +101,7 @@ def stats():
         )
     
     def stat_shell():
-        shell_pre_parse = "bash" #environ["SHELL"].replace("/bin/", "")
+        shell_pre_parse = environ["SHELL"].replace("/usr/bin/", "")
         shell_mid_parse = str(
             subprocess.check_output(
                 [f"{shell_pre_parse}", "--version"]
