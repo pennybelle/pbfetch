f293fe51b55792a7252e4cb6a024c1880a9c2000
kurealnum <oscar.gaske.cs@gmail.com>
2024-08-21 01:13:28 -0400

    Updated fix

--- src/pbfetch/main_funcs/stats.py
+++ src/pbfetch/main_funcs/stats.py
@@ -101,10 +101,7 @@ def stats():
         )
     
     def stat_shell():
-        try: 
-            shell_pre_parse = environ["SHELL"].replace("/bin/", "")
-        except FileNotFoundError:
-            shell_pre_parse = environ["SHELL"].replace("/usr/bin/", "")
+        shell_pre_parse = environ["SHELL"].split("/")[-1]
         shell_mid_parse = str(
             subprocess.check_output(
                 [f"{shell_pre_parse}", "--version"]
