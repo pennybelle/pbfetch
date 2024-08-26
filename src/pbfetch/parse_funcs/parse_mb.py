530508e0098682f86ea73cb6e4b03125cf336292
pennybelle <kylanfreeman98@gmail.com>
2024-08-26 14:35:06 -0400

    fix parse_mb.py

--- src/pbfetch/parse_funcs/parse_mb.py
+++ src/pbfetch/parse_funcs/parse_mb.py
@@ -2,15 +2,15 @@ from os import path
 
 
 def parse_mb():
-    file = path.join("sys", "class", "dmi", "id")
+    file = "/sys/class/dmi/id"
     try:
-        if path.isdir(path.join(file, "board_name")) is not True:
+        if path.exists(file + "/board_name") is not True:
             return "not found"
 
         with open(f"{file}/board_name") as mb:
             mb = mb.read().strip()
 
-        if path.isdir(path.join(file, "board_vendor")) is not True:
+        if path.exists(file + "/board_vendor") is not True:
             return mb
 
         with open(f"{file}/board_vendor") as v:
