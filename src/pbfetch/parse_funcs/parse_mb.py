29c56fad803fb80c947e59d614e6a2a2c54cf33d
pennybelle <contact@pennybelle.dev>
2024-08-25 21:33:11 -0400

    minor fix

--- src/pbfetch/parse_funcs/parse_mb.py
+++ src/pbfetch/parse_funcs/parse_mb.py
@@ -1,8 +1,17 @@
+from os import path
+
+
 def parse_mb():
-    file = "/sys/class/dmi/id"
+    file = path.join("sys", "class", "dmi", "id")
     try:
+        if path.isdir(path.join(file, "board_name")) is not True:
+            return "not found"
+
         with open(f"{file}/board_name") as mb:
             mb = mb.read().strip()
+
+        if path.isdir(path.join(file, "board_vendor")) is not True:
+            return mb
 
         with open(f"{file}/board_vendor") as v:
             v = v.read().strip()
@@ -10,5 +19,5 @@ def parse_mb():
         return f"{v} {mb}"
 
     except Exception as e:
-        print(e)
+        print(f"Parse Motherboard Error: {e}")
         return None
