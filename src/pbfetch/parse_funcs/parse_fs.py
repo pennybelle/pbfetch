1c687727c1680e472d5912de2cafaf4ed01ad0a4
pennybelle <kylanfreeman98@gmail.com>
2024-08-30 15:18:20 -0400

    better logic for fs parse

--- src/pbfetch/parse_funcs/parse_fs.py
+++ src/pbfetch/parse_funcs/parse_fs.py
@@ -1,4 +1,25 @@
+from psutil import disk_partitions
+
+
 def parse_fs():
+    try:
+        partitions = disk_partitions() 
+        disks = {}
+        for disk in partitions:
+            disks[disk.mountpoint] = disk
+            # print(disks)
+
+        if len(disks) == 1:
+            fs = disks.items()[0].fstype
+            return fs
+        
+        elif "/" in disks.keys():
+            fs = disks["/"].fstype
+            return fs
+
+    except Exception:
+        pass
+
     file = "/etc/fstab"
     try:
         with open(file) as fstab:
@@ -10,8 +31,8 @@ def parse_fs():
                     file_system = file_system.split()[0]
                     return file_system
 
-        return None
+        return "not found"
 
     except Exception as e:
-        print(e)
+        print(f"Parse Filesystem Error: {e}")
         return None
