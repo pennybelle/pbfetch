791ca282cfc40c8692a76b5837c33e5ac00927d1
pennybelle <kylanfreeman98@gmail.com>
2024-08-30 19:16:49 -0400

    add fallback for parsing font in tty

--- src/pbfetch/parse_funcs/parse_font.py
+++ src/pbfetch/parse_funcs/parse_font.py
@@ -18,6 +18,27 @@ def parse_font():
         else:
             return "not found"
 
+    except Exception:
+        pass
+
+    try:
+        font = Popen(
+            "zgrep FONT /proc/config.gz",
+            shell=True,
+            stdout=PIPE,
+            stderr=PIPE,
+        )
+        font = str(font.communicate()[0])
+        font = font[3 : len(font) - 4]
+
+        font = font.splitlines()
+        
+        for line in font[::-1]:
+            if "8x16=y" in line:
+                return "8x16"
+            elif "8x8=y" in line:
+                return "8x8"
+
     except Exception as e:
         print(f"Parse Font Error: {e}")
         return None
