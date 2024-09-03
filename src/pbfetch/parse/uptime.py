bc15b5be6fea8dfe35e12ea66069f4818ff64fdb
Federico Maria Morrone <contact@morrone.dev>
2024-09-03 10:24:48 +0200

    Refactor uptime parsing

--- src/pbfetch/parse/uptime.py
+++ src/pbfetch/parse/uptime.py
@@ -1,44 +1,17 @@
+import time
+
+
 def parse_uptime():
-    # parse uptime from /proc/uptime
-    with open("/proc/uptime", "r") as file:
-        seconds = int(float(file.readline().split()[0]))
+    seconds = int(time.clock_gettime(time.CLOCK_BOOTTIME))
 
     # math for time formatting with day, hr, min, sec
     m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
     d, h = divmod(h, 24)
 
-    # string formatting using calculated vals from above
-    days = f"{d:d}d"
-    hours = f"{h:d}h"
-    mins = f"{m:d}m"
-    secs = f"{s:d}s"
-    uptime = ""
+    fdays = "" if d == 0 else f"{d}d "
+    fhours = "" if h == 0 else f"{h}h "
+    fminutes = "" if m == 0 else f"{m}m "
+    fsecs = "" if s == 0 else f"{s}s "
 
-    # only join to uptime if uptime isnt empty
-    # else replace empty uptime with new value
-    def format_uptime(uptime, time):
-        if uptime:
-            uptime = " ".join([uptime, time])
-        else:
-            uptime = time
-
-        return uptime
-
-    # only show relevant data (ie: if day is 0, dont show day)
-    if d > 0:
-        uptime = format_uptime(uptime, days)
-
-    if h > 0:
-        uptime = format_uptime(uptime, hours)
-
-    if seconds > 60 and m > 0:
-        uptime = format_uptime(uptime, mins)
-
-    if s > 0:
-        uptime = format_uptime(uptime, secs)
-
-    if uptime != "":
-        return uptime
-    else:
-        return None
+    return (fdays + fhours + fminutes + fsecs).rstrip()
