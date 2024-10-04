7e2eebfd9860ee960adfe0b47b2951b0fad2f059
pennybelle <contact@pennybelle.dev>
2024-10-04 09:12:56 -0400

    fix fs parse with psutil

--- src/pbfetch/parse/fs.py
+++ src/pbfetch/parse/fs.py
@@ -6,17 +6,15 @@ def parse_fs():
         partitions = disk_partitions()
         disks = {}
         for disk in partitions:
-            disks[disk.device] = disk
-            # print(disks)
+            disks[disk.mountpoint] = disk.fstype
 
         values = [item for item in disks.values()]
         if len(values) == 1:
-            # print(values)
-            fs = values[0].fstype
+            fs = values[0]
             return fs
 
         elif "/" in disks.keys():
-            fs = disks["/"].fstype
+            fs = disks["/"]
             return fs
 
     except Exception:
@@ -38,3 +36,13 @@ def parse_fs():
     except Exception as e:
         print(f"Parse Filesystem Error: {e}")
         return None
+
+
+"""
+{'/dev/mapper/root': sdiskpart(device='/dev/mapper/root', mountpoint='/', fstype='btrfs', opts='rw,relatime,compress=zstd:3,ssd,space_cache=v2,subvolid=256,subvol=/@')}
+{'/dev/mapper/root': sdiskpart(device='/dev/mapper/root', mountpoint='/.snapshots', fstype='btrfs', opts='rw,relatime,compress=zstd:3,ssd,space_cache=v2,subvolid=260,subvol=/@.snapshots')}
+{'/dev/mapper/root': sdiskpart(device='/dev/mapper/root', mountpoint='/var/log', fstype='btrfs', opts='rw,relatime,compress=zstd:3,ssd,space_cache=v2,subvolid=258,subvol=/@log')}
+{'/dev/mapper/root': sdiskpart(device='/dev/mapper/root', mountpoint='/var/cache/pacman/pkg', fstype='btrfs', opts='rw,relatime,compress=zstd:3,ssd,space_cache=v2,subvolid=259,subvol=/@pkg')}
+{'/dev/mapper/root': sdiskpart(device='/dev/mapper/root', mountpoint='/home', fstype='btrfs', opts='rw,relatime,compress=zstd:3,ssd,space_cache=v2,subvolid=257,subvol=/@home')}
+{'/dev/mapper/root': sdiskpart(device='/dev/mapper/root', mountpoint='/home', fstype='btrfs', opts='rw,relatime,compress=zstd:3,ssd,space_cache=v2,subvolid=257,subvol=/@home'), '/dev/nvme0n1p1': sdiskpart(device='/dev/nvme0n1p1', mountpoint='/boot', fstype='vfat', opts='rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=ascii,shortname=mixed,utf8,errors=remount-ro')}
+"""
