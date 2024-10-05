dac032ac5d195ff7cd941c6aa156e96fa7c3c16f
pennybelle <contact@pennybelle.dev>
2024-10-05 14:16:56 -0400

    fix verbose parsing for gpu name

--- src/pbfetch/parse/gpu.py
+++ src/pbfetch/parse/gpu.py
@@ -31,7 +31,7 @@ def parse_gpu():
         if gpu_name:
             gpu_name = gpu_name.split(": ")[1]
             gpu_name = gpu_name.strip()
-            return sub(r"\(.+\)$", "", gpu_name.strip())
+            return sub(r" \(.+\)$", "", gpu_name.strip())
 
     except Exception:
         pass
@@ -46,7 +46,7 @@ def parse_gpu():
         gpu_name = gpu_name.replace("OpenGL compatibility profile renderer: ", "")
         # print(gpu_name)
 
-        return sub(r"\(.+\)$", "", gpu_name.strip())
+        return sub(r" \(.+\)$", "", gpu_name.strip())
 
     except Exception:
         pass
@@ -64,7 +64,7 @@ def parse_gpu():
         gpu_name = gpu_name[2 : len(gpu_name) - 3]
         gpu_name = gpu_name.split("VGA compatible controller:")[1].strip()
 
-        return sub(r"\(.+\)$", "", gpu_name.strip())
+        return sub(r" \(.+\)$", "", gpu_name.strip())
 
     except Exception as e:
         print(f"GPU Parse Error: {e}")
