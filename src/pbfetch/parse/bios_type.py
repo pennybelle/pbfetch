cdcc54309e5adf8ce9fdc55f13bf6615116aac75
pennybelle <contact@pennybelle.dev>
2024-10-06 07:08:57 -0400

    include bios vendor and version

--- src/pbfetch/parse/bios_type.py
+++ src/pbfetch/parse/bios_type.py
@@ -2,6 +2,18 @@ from os import path
 
 
 def parse_bios_type():
+    # try:
+    #     with open("/sys/class/dmi/id/bios_vendor") as vendor:
+    #         vendor = vendor.read().strip()
+
+    #     with open("/sys/class/dmi/id/bios_version") as version:
+    #         version = version.read().strip()
+
+    #     return f"{vendor} {version}"
+
+    # except Exception:
+    #     pass
+
     try:
         if path.isdir("/sys/firmware/efi") or path.exists(
             "/sys/firmware/acpi/tables/UEFI"
@@ -9,6 +21,20 @@ def parse_bios_type():
             bios_type = "UEFI"
         else:
             bios_type = "Legacy"
+
+        vendor_path = "/sys/class/dmi/id/bios_vendor"
+        if path.exists(vendor_path):
+            with open(vendor_path) as vendor:
+                vendor = vendor.read().strip()
+                bios_type += f" {vendor}"
+
+        vendor_version = "/sys/class/dmi/id/bios_version"
+        if path.exists(vendor_version):
+            with open(vendor_version) as version:
+                version = version.read().strip()
+                bios_type += f" {version}"
+
+        # return f"{vendor} {version}"
 
         return bios_type
 
