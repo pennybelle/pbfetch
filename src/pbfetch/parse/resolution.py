5e94dcf5e5135992faf8323a2ba0816153ac576a
pennybelle <contact@pennybelle.dev>
2024-10-30 09:24:20 -0400

    add support for tty resolution detection

--- src/pbfetch/parse/resolution.py
+++ src/pbfetch/parse/resolution.py
@@ -1,19 +1,24 @@
 from os import path
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, run
 
 import tkinter as tk
 
 
-def command(input):
-    output = Popen(
-        input,
-        shell=True,
-        stdout=PIPE,
-        stderr=PIPE,
-    )
-    output = str(output.communicate()[0])[2 : len(output) - 1]
+def command(input, index):
+    try:
+        output = Popen(
+            input,
+            shell=True,
+            stdout=PIPE,
+            stderr=PIPE,
+        )
+        output = str(output.communicate()[index])
+        output = output[2 : len(output) - 1]
 
-    return output
+        return output
+
+    except Exception:
+        pass
 
 
 def parse_res():
@@ -23,24 +28,31 @@ def parse_res():
         w = root.winfo_screenwidth()
         h = root.winfo_screenheight()
 
-        return f"{w}x{h}"
+        res = f"{w}x{h}"
+        if res and w and h:
+            return res
+        else:
+            pass
 
     except Exception:
         pass
 
+    # try:
+    #     res = command("screenresolution get 2>&1", 0)
+
+    #     return res
+
+    # except Exception:
+    #     # print("screenresolution failed")
+    #     pass
+
     try:
-        res = command("screenresolution get 2>&1")
+        res = command("system_profiler SPDisplaysDataType", 0)
 
-        return res
-
-    except Exception:
-        # print("screenresolution failed")
-        pass
-
-    try:
-        res = command("system_profiler SPDisplaysDataType")
-
-        return res
+        if res:
+            return res
+        else:
+            pass
 
     except Exception:
         # print("system_profiler failed")
@@ -48,10 +60,14 @@ def parse_res():
 
     try:
         res = command(
-            """PlistBuddy -c "Print DisplayAnyUserSets:0:0:Resolution" /Library/Preferences/com.apple.windowserver.plist"""
+            """PlistBuddy -c "Print DisplayAnyUserSets:0:0:Resolution" /Library/Preferences/com.apple.windowserver.plist""",
+            0,
         )
 
-        return res
+        if res:
+            return res
+        else:
+            pass
 
     except Exception:
         # print("PlistBuddy failed")
@@ -59,56 +75,90 @@ def parse_res():
 
     try:
         h = command(
-            """wmic path Win32_VideoController get CurrentHorizontalResolution"""
+            """wmic path Win32_VideoController get CurrentHorizontalResolution""", 0
         )
         w = command(
-            """wmic path Win32_VideoController get CurrentVerticalResolution)"""
+            """wmic path Win32_VideoController get CurrentVerticalResolution)""", 0
         )
 
         res = f"{w}x{h}"
 
-        return res
+        if res and w and h:
+            return res
+        else:
+            pass
 
     except Exception:
         # print("wmic failed")
         pass
 
     try:
-        res = command("screenmode")
+        res = command("screenmode", 0)
 
-        return res
+        if res:
+            return res
+        else:
+            pass
 
     except Exception:
         # print("screenmode failed")
         pass
 
     try:
-        res = command("xrandr --nograb --current")
+        res = command("xrandr --nograb --current", 0)
 
-        return res
+        if res:
+            return res
+        else:
+            pass
 
     except Exception:
         # print("xrandr failed")
         pass
 
     try:
-        res = command("xwininfo -root")
+        res = command("xwininfo -root", 0)
 
-        return res
+        if res:
+            return res
+        else:
+            pass
 
     except Exception:
         pass
 
     try:
-        res = command("xdpyinfo")
+        res = command("xdpyinfo", 0)
 
-        return res
+        if res:
+            return res
+        else:
+            pass
 
     except Exception:
         # print("xdpyinfo failed")
         pass
 
-    res_path = "/sys/class/drm"
+    try:
+        font_size = command("setfont -v", 1)
+        font_size = font_size.split("font from file")[0]
+        font_size = font_size.split()[-1]
+        font_x, font_y = tuple(map(int, font_size.split("x")))
+        term_size = command("stty size", 0).replace(r"\n", "")
+        y, x = tuple(map(int, term_size.split()))
+
+        res_x = x * font_x
+        res_y = y * font_y
+
+        res = f"{res_x}x{res_y}"
+
+        if res and res_x and res_y:
+            return res
+        else:
+            pass
+
+    except Exception:
+        pass
 
     # print("all failed, falling back to eDP or LVDS")
 
@@ -146,6 +196,8 @@ def parse_res():
 
     # except Exception as e:
     #     print(e)
+
+    res_path = "/sys/class/drm"
 
     try:
         display_types = ["LVDS", "eDP"]
