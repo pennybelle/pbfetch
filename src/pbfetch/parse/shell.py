33256b6b778b614f35f91056885027657776cb63
pennybelle <contact@pennybelle.dev>
2024-12-27 02:36:41 -0500

    handle shell errors

--- src/pbfetch/parse/shell.py
+++ src/pbfetch/parse/shell.py
@@ -3,17 +3,22 @@ from subprocess import check_output
 
 
 def parse_shell():
-    shell_pre_parse = path.realpath(f"/proc/{getppid()}/exe")
-    shell_name = shell_pre_parse.split("/")[-1]
-    shell_name = shell_name.split()[0]
-    shell_version_pre_parse = str(
-        check_output([f"{shell_name}", "--version"]).decode("utf-8")
-    )
-    if shell_name == "bash":
-        shell_version = shell_version_pre_parse.split()[3]
-    elif shell_name == "zsh":
-        shell_version = shell_version_pre_parse.split()[1]
-    else:
-        return str(shell_name)
+    try:
+        shell_pre_parse = path.realpath(f"/proc/{getppid()}/exe")
+        shell_name = shell_pre_parse.split("/")[-1]
+        shell_name = shell_name.split()[0]
+        shell_version_pre_parse = str(
+            check_output([f"{shell_name}", "--version"]).decode("utf-8")
+        )
+        if shell_name == "bash":
+            shell_version = shell_version_pre_parse.split()[3]
+        elif shell_name == "zsh":
+            shell_version = shell_version_pre_parse.split()[1]
+        else:
+            return str(shell_name)
 
-    return f"{shell_name} {shell_version}"
+        return f"{shell_name} {shell_version}"
+
+    except Exception as e:
+        print(f"Parse Shell Error: {e}")
+        return None
