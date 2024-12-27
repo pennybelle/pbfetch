12a6e5db536c099b4930e5c7f1efe51167408e45
pennybelle <contact@pennybelle.dev>
2024-12-27 02:34:20 -0500

    handle temp errors

--- src/pbfetch/parse/temp.py
+++ src/pbfetch/parse/temp.py
@@ -1,4 +1,9 @@
 # parse & format temp from /sys/class/thermal/thermal_zone0/temp
 def parse_temp():
-    with open("/sys/class/thermal/thermal_zone0/temp") as temp:
-        return round(int(temp.read()) / 1000)
+    try:
+        with open("/sys/class/thermal/thermal_zone0/temp") as temp:
+            return round(int(temp.read()) / 1000)
+
+    except Exception as e:
+        print(f"Parse Temp Error: {e}")
+        return None
