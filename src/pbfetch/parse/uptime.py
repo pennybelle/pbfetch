77d411b1af61daa1d4400bb8eac090d0c3732c21
pennybelle <contact@pennybelle.dev>
2024-12-27 02:27:09 -0500

    handle uptime errors

--- src/pbfetch/parse/uptime.py
+++ src/pbfetch/parse/uptime.py
@@ -2,16 +2,21 @@ from time import clock_gettime, CLOCK_BOOTTIME
 
 
 def parse_uptime():
-    seconds = int(clock_gettime(CLOCK_BOOTTIME))
+    try:
+        seconds = int(clock_gettime(CLOCK_BOOTTIME))
 
-    # math for time formatting with day, hr, min, sec
-    m, s = divmod(seconds, 60)
-    h, m = divmod(m, 60)
-    d, h = divmod(h, 24)
+        # math for time formatting with day, hr, min, sec
+        m, s = divmod(seconds, 60)
+        h, m = divmod(m, 60)
+        d, h = divmod(h, 24)
 
-    fdays = f"{d}d " if d > 0 else ""
-    fhours = f"{h}h " if h > 0 else ""
-    fminutes = f"{m}m " if m > 0 else ""
-    fsecs = f"{s}s " if s > 0 else ""
+        fdays = f"{d}d " if d > 0 else ""
+        fhours = f"{h}h " if h > 0 else ""
+        fminutes = f"{m}m " if m > 0 else ""
+        fsecs = f"{s}s " if s > 0 else ""
 
-    return (fdays + fhours + fminutes + fsecs).rstrip()
+        return (fdays + fhours + fminutes + fsecs).rstrip()
+
+    except Exception as e:
+        print(f"Uptime Error: {e}")
+        return None
