48b02c687a42fd7acea7bdb94be9e4934bfeb1a1
pennybelle <contact@pennybelle.dev>
2024-12-27 04:43:48 -0500

    fix handling shell version

--- src/pbfetch/parse/shell.py
+++ src/pbfetch/parse/shell.py
@@ -3,22 +3,26 @@ from subprocess import check_output
 
 
 def parse_shell():
+    check_version = ["bash", "zsh"]
     try:
         shell_pre_parse = path.realpath(f"/proc/{getppid()}/exe")
         shell_name = shell_pre_parse.split("/")[-1]
         shell_name = shell_name.split()[0]
+
+    except Exception as e:
+        print(f"Parse Shell Error: {e}")
+        return None
+
+    try:
         shell_version_pre_parse = str(
             check_output([f"{shell_name}", "--version"]).decode("utf-8")
         )
-        if shell_name == "bash":
+        if shell_name in check_version:
             shell_version = shell_version_pre_parse.split()[3]
-        elif shell_name == "zsh":
-            shell_version = shell_version_pre_parse.split()[1]
         else:
             return str(shell_name)
 
         return f"{shell_name} {shell_version}"
 
-    except Exception as e:
-        print(f"Parse Shell Error: {e}")
-        return None
+    except Exception:
+        return shell_name
